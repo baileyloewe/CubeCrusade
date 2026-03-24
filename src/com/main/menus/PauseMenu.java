3279333a5ae721a1e9432c14ce2410f34a75cf91
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2026-03-24 13:42:13 -0400

    Chore: Refactor state handler
    Refactored state handler class into 4 distinct pieces, a graphics util class, a menu manager, menu class, and children menu classes.

---
+++ src/com/main/menus/PauseMenu.java
@@ -0,0 +1,44 @@
+package com.main.menus;
+
+import com.main.*;
+
+import java.awt.*;
+
+import static com.main.GraphicsUtil.*;
+
+public class PauseMenu extends Menu {
+    private final MenuBoxItem pausedOutlineBox, resumeBox, settingsBox, mainMenuBox, exitGameBox;
+
+    public PauseMenu(MenuManager menuManager) {
+        super(menuManager);
+        pausedOutlineBox = new MenuBoxItem(centeredX - 100, centeredY - 210, 200, 70, "PAUSED");
+        resumeBox = new MenuBoxItem(centeredX - 60, centeredY - 92, 120, 44, "RESUME");
+        settingsBox = new MenuBoxItem(centeredX - 80, centeredY + 3, 160, 44, "SETTINGS");
+        mainMenuBox = new MenuBoxItem(centeredX - 90, centeredY + 98, 180, 44, "MAIN MENU");
+        exitGameBox = new MenuBoxItem(centeredX - 90, centeredY + 193, 180, 44, "EXIT GAME");
+    }
+
+    public void render(Graphics g) {
+        drawRectAndString(g, pausedOutlineBox, Fonts.LARGE);
+        drawRectAndString(g, resumeBox, Fonts.MEDIUM);
+        drawRectAndString(g, settingsBox, Fonts.MEDIUM);
+        drawRectAndString(g, mainMenuBox, Fonts.MEDIUM);
+        drawRectAndString(g, exitGameBox, Fonts.MEDIUM);
+    }
+
+    public void interact(int mouseX, int mouseY) {
+        if (mouseOverItem(resumeBox, mouseX, mouseY)) {
+            mediator.getGame().gameState = Game.STATE.Game;
+        } else if (mouseOverItem(settingsBox, mouseX, mouseY)) {
+            mediator.getGame().gameState = Game.STATE.Settings;
+        } else if (mouseOverItem(exitGameBox, mouseX, mouseY)) {
+            mediator.getGame().exitGame();
+        } else if (mouseOverItem(mainMenuBox, mouseX, mouseY)) {
+            mm.gameLive = false;
+            handler.clearAll();
+            mediator.getGame().gameState = Game.STATE.Menu;
+            changeAudio();
+            new MenuParticle(mouseX, mouseY, ID.MenuParticle, handler);
+        }
+    }
+}
---
+++ src/com/main/menus/ShopMenu.java
@@ -0,0 +1,61 @@
+package com.main.menus;
+
+import com.main.*;
+
+import java.awt.*;
+
+public class ShopMenu extends Menu {
+    private final MenuBoxItem shopShopBox, shopUpgradeHealthBox, shopUpgradeSpeedBox, shopRefillHealthBox, shopCurrencyBox, shopBackBox;
+
+    public ShopMenu(MenuManager menuManager) {
+        super(menuManager);
+
+        shopShopBox = new MenuBoxItem(centeredX - 75, centeredY - 200, 150, 70, "SHOP");
+        shopUpgradeHealthBox = new MenuBoxItem(centeredX - 140, centeredY - 82, 280, 44, "UPGRADE HEALTH (" + upgrade.getCostOfNextHealthUpgrade() + ")");
+        shopUpgradeSpeedBox = new MenuBoxItem(centeredX - 130, centeredY + 13, 260, 44, "UPGRADE SPEED (" + upgrade.getCostOfNextSpeedUpgrade() + ")");
+        shopRefillHealthBox = new MenuBoxItem(centeredX - 130, centeredY + 108, 260, 44, "REFILL HEALTH");
+        shopCurrencyBox = new MenuBoxItem(centeredX - 130, centeredY + 183, 260, 44, "GOLD: " + (int) upgrade.getPlayerCurrency());
+        shopBackBox = new MenuBoxItem(centeredX - 40, centeredY + 278, 80, 44, "BACK");
+    }
+
+    public void updateShopBoxSizesAndText() {
+        shopUpgradeHealthBox.text = "UPGRADE HEALTH (" + upgrade.getCostOfNextHealthUpgrade() + ")";
+
+        shopUpgradeHealthBox.rect.x = centeredX - (shopUpgradeHealthBox.text.length() * 10);
+        shopUpgradeHealthBox.rect.width = (shopUpgradeHealthBox.text.length() * 20);
+
+        shopUpgradeSpeedBox.text = "UPGRADE SPEED (" + upgrade.getCostOfNextSpeedUpgrade() + ")";
+        shopUpgradeSpeedBox.rect.x = centeredX - (shopUpgradeSpeedBox.text.length() * 10);
+        shopUpgradeSpeedBox.rect.width = (shopUpgradeSpeedBox.text.length() * 20);
+
+        shopCurrencyBox.text = "GOLD: " + (int) upgrade.getPlayerCurrency();
+        shopCurrencyBox.rect.x = centeredX - (shopCurrencyBox.text.length() * 10);
+        shopCurrencyBox.rect.width = (shopCurrencyBox.text.length() * 20);
+
+        shopRefillHealthBox.text = "REFILL HEALTH (" + upgrade.getCostOfNextHealthRefill() + ")";
+        shopRefillHealthBox.rect.x = centeredX - (shopRefillHealthBox.text.length() * 10);
+        shopRefillHealthBox.rect.width = (shopRefillHealthBox.text.length() * 20);
+    }
+
+    public void interact(int mouseX, int mouseY) {
+        if (mouseOverItem(shopUpgradeHealthBox, mouseX, mouseY)) {
+            upgrade.buyHealthUpgrade();
+        } else if (mouseOverItem(shopUpgradeSpeedBox, mouseX, mouseY)) {
+            upgrade.buySpeedUpgrade();
+        } else if (mouseOverItem(shopRefillHealthBox, mouseX, mouseY)) {
+            upgrade.buyHealthRefill();
+        } else if (mouseOverItem(shopBackBox, mouseX, mouseY)) {
+            mediator.getGame().gameState = Game.STATE.Game;
+        }
+    }
+
+    public void render(Graphics g) {
+        GraphicsUtil.drawRectAndString(g, shopShopBox, GraphicsUtil.Fonts.LARGE);
+        GraphicsUtil.drawRectAndString(g, shopUpgradeHealthBox, GraphicsUtil.Fonts.MEDIUM);
+        GraphicsUtil.drawRectAndString(g, shopUpgradeSpeedBox, GraphicsUtil.Fonts.MEDIUM);
+        GraphicsUtil.drawRectAndString(g, shopRefillHealthBox, GraphicsUtil.Fonts.MEDIUM);
+        GraphicsUtil.drawRectAndString(g, shopCurrencyBox, GraphicsUtil.Fonts.MEDIUM);
+        GraphicsUtil.drawRectAndString(g, shopBackBox, GraphicsUtil.Fonts.MEDIUM);
+        updateShopBoxSizesAndText();
+    }
+}
