fdab80d56da32435e73fc0d423d6757bd39ee984
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2026-03-26 09:48:59 -0400

    chore: refactor file structure

---
+++ src/main/java/com/github/baileyloewe/cubecrusade/Upgrade.java
@@ -0,0 +1,149 @@
+package com.github.baileyloewe.cubecrusade;
+
+import com.github.baileyloewe.cubecrusade.signals.GameSignals;
+
+public class Upgrade {
+
+    private int healthUpgradeCount = 1;
+    private int speedUpgradeCount = 1;
+    private int healthRefillCount = 1;
+    private int score;
+    private int level;
+    private final float baseSpeed = 2;
+    private float currentSpeed;
+    private float playerCurrency;
+
+    public Upgrade() {
+        GameSignals.GameStarted.connect(this::initializeValues);
+        GameSignals.HealthUpgradePurchased.connect(this::buyHealthUpgrade);
+        GameSignals.SpeedUpgradePurchased.connect(this::buySpeedUpgrade);
+        GameSignals.HealthRefillPurchased.connect(this::buyHealthRefill);
+    }
+
+    public int getHealthUpgradeCount() {
+        return healthUpgradeCount;
+    }
+
+    public void setHealthUpgradeCount(int healthUpgradeCount) {
+        this.healthUpgradeCount = healthUpgradeCount;
+    }
+
+    public void incrementHealthUpgrades() {
+        this.healthUpgradeCount += 1;
+    }
+
+    public int getCostOfNextHealthUpgrade() {
+        return 100 * (getHealthUpgradeCount());
+    }
+
+    public void buyHealthUpgrade() {
+        if (playerCurrency > getCostOfNextHealthUpgrade()) {
+            playerCurrency -= getCostOfNextHealthUpgrade();
+            incrementHealthUpgrades();
+            GameSignals.baseHealthIncreased.emit(25);
+        }
+    }
+
+    public int getSpeedUpgradeCount() {
+        return speedUpgradeCount;
+    }
+
+    public void setSpeedUpgradeCount(int speedUpgradeCount) {
+        this.speedUpgradeCount = speedUpgradeCount;
+    }
+
+    public void incrementSpeedUpgradeCount() {
+        this.speedUpgradeCount += 1;
+    }
+
+    public int getCostOfNextSpeedUpgrade() {
+        return 100 * (getSpeedUpgradeCount());
+    }
+
+    public void buySpeedUpgrade() {
+        if (playerCurrency > getCostOfNextSpeedUpgrade()) {
+            playerCurrency -= getCostOfNextSpeedUpgrade();
+            incrementSpeedUpgradeCount();
+            currentSpeed = 2 + ((speedUpgradeCount - 1) * .1f);
+        }
+    }
+
+    public int getHealthRefills() {
+        return healthRefillCount;
+    }
+
+    public void setHealthRefills(int healthRefills) {
+        this.healthRefillCount = healthRefills;
+    }
+
+    public int getCostOfNextHealthRefill() {
+        return 100 * (getHealthRefills());
+    }
+
+    public void incrementHealthRefills() {
+        this.healthRefillCount += 1;
+    }
+
+    public void buyHealthRefill() {
+        if (playerCurrency > getCostOfNextHealthRefill()) {
+            playerCurrency -= getCostOfNextHealthRefill();
+            incrementHealthRefills();
+            GameSignals.healthRefilled.emit();
+        }
+    }
+
+    public void initializeValues() {
+        setCurrentSpeed(getBaseSpeed());
+        setLevel(1);
+        setScore(0);
+        setPlayerCurrency(0);
+        setHealthUpgradeCount(1);
+        setSpeedUpgradeCount(1);
+        setHealthRefills(1);
+    }
+
+    public float getPlayerCurrency() {
+        return playerCurrency;
+    }
+
+    public void setPlayerCurrency(float playerCurrency) {
+        this.playerCurrency = playerCurrency;
+    }
+
+    public void addPlayerCurrency(float currency) {
+        playerCurrency += currency;
+    }
+
+    public float getBaseSpeed() {
+        return baseSpeed;
+    }
+
+    public float getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setCurrentSpeed(float currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void incrementScore() {
+        score += 1;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+}
---
+++ src/main/java/com/github/baileyloewe/cubecrusade/menus/GameOverMenu.java
@@ -0,0 +1,35 @@
+package com.github.baileyloewe.cubecrusade.menus;
+
+import com.github.baileyloewe.cubecrusade.ID;
+import com.github.baileyloewe.cubecrusade.MenuBoxItem;
+import com.github.baileyloewe.cubecrusade.MenuParticle;
+import com.github.baileyloewe.cubecrusade.signals.GameSignals;
+
+import java.awt.*;
+import static com.github.baileyloewe.cubecrusade.GraphicsUtil.*;
+
+public class GameOverMenu extends Menu {
+    private final MenuBoxItem mainMenuBox, gameOverBox;
+
+    public GameOverMenu(MenuManager menuManager) {
+        super(menuManager);
+        gameOverBox = new MenuBoxItem(centeredX - 175, centeredY - 170, 350, 70, "GAME OVER");
+        mainMenuBox = new MenuBoxItem(centeredX - 90, centeredY + 163, 180, 44, "MAIN MENU");
+    }
+
+    public void interact(int mouseX, int mouseY) {
+        if (mouseOverItem(mainMenuBox, mouseX, mouseY)) {
+            GameSignals.GameQuit.emit();
+            new MenuParticle(mouseX, mouseY, ID.MenuParticle, handler);
+        }
+    }
+
+    public void render(Graphics g) {
+        handler.clearAll();
+        drawRectAndString(g, gameOverBox, Fonts.XLARGE);
+        g.setFont(Fonts.SMALL.getFont());
+        g.drawString("You lost with a score of " + upgrade.getScore() + "\n on level " + upgrade.getLevel(), centeredX - 180, centeredY);
+        drawRectAndString(g, mainMenuBox, Fonts.MEDIUM);
+    }
+
+}
