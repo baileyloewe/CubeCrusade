03a62512b308f26b844d58719ec4b5abf13bb5ce
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2026-03-31 14:45:22 -0400

    refactor: added clamping to prevent out of bounds positions

--- src/main/java/com/github/baileyloewe/cubecrusade/entities/components/PositionComponent.java
+++ src/main/java/com/github/baileyloewe/cubecrusade/entities/components/PositionComponent.java
@@ -1,5 +1,6 @@
 package com.github.baileyloewe.cubecrusade.entities.components;
 
+import com.github.baileyloewe.cubecrusade.Game;
 import com.github.baileyloewe.cubecrusade.Vector2D;
 
 public class PositionComponent {
@@ -7,5 +8,10 @@ public class PositionComponent {
 
     public PositionComponent(Vector2D position) {
         this.position = position;
+    }
+
+    public void clamp(SizeComponent sizeComponent) {
+        position.x = Math.clamp(position.x, 0, Game.WIDTH - sizeComponent.width);
+        position.y = Math.clamp(position.y, 0, Game.HEIGHT - sizeComponent.height);
     }
 }
