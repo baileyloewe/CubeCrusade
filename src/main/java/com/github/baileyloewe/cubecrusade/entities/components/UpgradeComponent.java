5f5b192f1621cb0a147239f3f3314dd2c0761585
Bailey Loewe <72360204+baileyloewe@users.noreply.github.com>
2026-03-31 13:12:04 -0400

    Create UpgradeComponent.java

---
+++ src/main/java/com/github/baileyloewe/cubecrusade/entities/components/UpgradeComponent.java
@@ -0,0 +1,59 @@
+package com.github.baileyloewe.cubecrusade.entities.components;
+
+import com.github.baileyloewe.cubecrusade.entities.Player;
+import com.github.baileyloewe.cubecrusade.signals.GameSignals;
+import com.github.baileyloewe.cubecrusade.signals.Signals.EventSignal;
+import com.github.baileyloewe.cubecrusade.signals.Signals.Signal;
+
+public class UpgradeComponent {
+    private final Player player;
+    private int healthUpgradeCount = 1;
+    private int speedUpgradeCount = 1;
+    private int healthRefillCount = 1;
+    public final Signal<Integer> baseHealthIncreased = new Signal<>();
+    public final EventSignal healthRefilled = new EventSignal();
+    public final Signal<Float> speedIncreased = new Signal<>();
+
+    public UpgradeComponent(Player player) {
+        this.player = player;
+        GameSignals.healthUpgradePurchased.connect(this::buyHealthUpgrade);
+        GameSignals.speedUpgradePurchased.connect(this::buySpeedUpgrade);
+        GameSignals.healthRefillPurchased.connect(this::buyHealthRefill);
+    }
+
+    public int getCostOfNextHealthUpgrade() {
+        return 100 * (healthUpgradeCount + 1);
+    }
+
+    public void buyHealthUpgrade() {
+        if (player.getGold() > getCostOfNextHealthUpgrade()) {
+            player.removeGold(getCostOfNextHealthUpgrade());
+            healthUpgradeCount++;
+            baseHealthIncreased.emit(25);
+        }
+    }
+
+    public int getCostOfNextSpeedUpgrade() {
+        return 100 * (speedUpgradeCount + 1);
+    }
+
+    public void buySpeedUpgrade() {
+        if (player.getGold() > getCostOfNextSpeedUpgrade()) {
+            player.removeGold(getCostOfNextSpeedUpgrade());
+            speedUpgradeCount += 1;
+            speedIncreased.emit(speedUpgradeCount * .1f);
+        }
+    }
+
+    public int getCostOfNextHealthRefill() {
+        return 100 * (healthRefillCount + 1);
+    }
+
+    public void buyHealthRefill() {
+        if (player.getGold() > getCostOfNextHealthRefill()) {
+            player.removeGold(getCostOfNextHealthRefill());
+            healthRefillCount += 1;
+            healthRefilled.emit();
+        }
+    }
+}
